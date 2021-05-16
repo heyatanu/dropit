72be3724ffd5658df9f8913fc56d950b9559f97a
Atanu <atanud662@gmail.com>
2021-05-17 01:10:57 +0530

    First Upload

---
+++ FromUrlOpen.js
@@ -0,0 +1,32 @@
+var url_string = window.location.href
+var url = new URL(url_string);
+var id = url.searchParams.get("id");
+let search = url.search
+let slicestr = search.slice(1, 3)
+
+if (search.includes(",") || slicestr != "id" && search != "") {
+  document.getElementById("link-status-txt").innerHTML = "Wrong URL, URL should be " + url.origin + "/?id=your ID"
+  document.getElementById("fromlinkbtn").click()
+} else if (id != null) {
+  document.body.style.overflow = "hidden";
+  document.getElementById("overlayforurldownload").style.display = "block";
+  firebase.database().ref('Picture/' + id).on('value', function(snapshot) {
+
+    if (snapshot.val() != null) {
+      document.getElementById("overlayforurldownload").style.display = "none";
+      document.body.style.overflow = "auto";
+      let aclink = (snapshot.val().Link)
+      window.location.replace(url.origin)
+      window.open(aclink)
+
+    } else {
+      document.body.style.overflow = "auto";
+      document.getElementById("link-status-txt").innerHTML = "ID not found Check the ID"
+      document.getElementById("fromlinkbtn").click()
+      document.getElementById("overlayforurldownload").style.display = "none";
+    }
+    
+  });
+
+
+}
---
+++ InitializeFirebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCs6schJHYqg8Kreg1CiTTvcDMg_KVVKLc",
+    authDomain: "image-e2363.firebaseapp.com",
+    databaseURL: "https://image-e2363-default-rtdb.firebaseio.com",
+    projectId: "image-e2363",
+    storageBucket: "image-e2363.appspot.com",
+    messagingSenderId: "729926562573",
+    appId: "1:729926562573:web:bc69d7e95e9d3e1eb9f644",
+    measurementId: "G-4S4FRB6XRZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
