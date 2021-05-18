3db9df3b19d9a5cdd2132f19976d6e19e583761e
Atanu <atanud662@gmail.com>
2021-05-18 15:44:47 +0530

    Update DeleteData.js

--- DeleteData.js
+++ DeleteData.js
@@ -1,13 +1,14 @@
+
 document.getElementById("deletedata-btn").onclick = function() {
     document.getElementById("deletedata-btn").disabled = true;
-    let fileid = document.getElementById("deletedata-id").value;
-    let fileex = document.getElementById("deletedata-ex").value;
-    let deletedataststus = document.getElementById("deletedata-sts");
-    fileid = fileid.trim()
-    fileex = fileex.trim()
-    fileex = fileex.toLowerCase();
-    let filenameui = fileid + "." + fileex;
-
+let fileid = document.getElementById("deletedata-id").value;
+let fileex = document.getElementById("deletedata-ex").value;
+let deletedataststus = document.getElementById("deletedata-sts");
+fileid = fileid.trim()
+fileex = fileex.trim()
+fileex = fileex.toLowerCase();
+let boolb=false;
+let filenameui = fileid + "." + fileex;
     if (fileex == "" || fileid == "") {
         //NOT VALID
         // console.log("NOT VALID")
@@ -25,29 +26,46 @@ document.getElementById("deletedata-btn").onclick = function() {
                     desertRef.delete().then(() => {
                         deletedataststus.innerHTML = "deleted successfull";
                         // console.log("DLETE FILE ")
+                        boolb=funcall(true,fileid)
                         fileid = "";
                         fileex = "";
                         document.getElementById("deletedata-btn").disabled = false;
                     }).catch((error) => {
                         // console.log("SOME EROR OCC")
-                        deletedataststus.innerHTML = "we are down try later"
-                        document.getElementById("deletedata-btn").disabled = false;
+                        if (boolb==false){
+                            deletedataststus.innerHTML = "deleted successfull";
+                            document.getElementById("deletedata-btn").disabled = false;
+                        }
                     });
-                    firebase.database().ref('Picture/' + fileid).remove();
 
                 } else {
                     //WRONG EXTENTION
                     // console.log("NOT EX")
-                    deletedataststus.innerHTML = "check the file extention"
-                    document.getElementById("deletedata-btn").disabled = false;
+                    if (boolb==false){
+                        deletedataststus.innerHTML = "check the file extention"
+                        document.getElementById("deletedata-btn").disabled = false;
+                    }
                 }
             } else {
                 //NOT FOUND
                 // console.log("NOT FOUND")
-                deletedataststus.innerHTML = "file not found"
-                document.getElementById("deletedata-btn").disabled = false;
+                if (boolb==false){
+                    deletedataststus.innerHTML = "file not found"
+                    document.getElementById("deletedata-btn").disabled = false;
+                }
             }
         });
 
     }
 }
+
+function funcall(b,c){
+    if (b){
+        firebase.database().ref('Picture/' + c).remove();
+        // console.log(")))")
+    retuen (true);
+    }
+} 
+
+console.disableYellowBox = true;
+console.reportErrorsAsExceptions = false;
