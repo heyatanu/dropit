0035be9bf1a17b5c3952a07bcf18cec86c0babef
Atanu <atanud662@gmail.com>
2021-05-28 00:35:20 +0530

    Update script.js

--- ForgotAccessID/script.js
+++ ForgotAccessID/script.js
@@ -1,6 +1,6 @@
-// setTimeout(function(){console.clear();}, 5000);
+setTimeout(function(){console.clear();}, 5000);
 
 
-// setInterval(function(){ console.clear();}, 18000);
-// document.addEventListener('contextmenu', event => event.preventDefault());
-// document.ondragstart = function() { return false; };
+setInterval(function(){ console.clear();}, 18000);
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.ondragstart = function() { return false; };
